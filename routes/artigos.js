22bc805f3133d8b8ecb708354b31324b0874e8e8
MT <miguelangelo_teixeira@hotmail.com>
2017-11-08 17:45:58 +0000

    Adicionado lib requests. Teste de pedido ao backend na VM

--- routes/artigos.js
+++ routes/artigos.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/artigos/index');
+  request('http://192.168.43.22:49822/api/artigos/',{json:true},function(err, response, body)
+  {
+    //if(err)
+      //{res.status(400)};
+    console.log(body);
+    console.log(err);
+    res.render('pages/artigos/index', {artigos: body});
+  })
 });
 
 module.exports = router;
