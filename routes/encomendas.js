dd60c2527d427511d808d5ca06d7f6e991951b01
Telmo Barros <telmo.20@hotmail.com>
2017-12-11 22:46:16 +0000

    identation fix e encomendas

--- routes/encomendas.js
+++ routes/encomendas.js
@@ -1,22 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 /* GET lista todos as encomendas. */
 router.get('/', function (req, res, next) {
-    if (req.session.login) {
-        res.render('pages/encomendas/index');
-    } else {
+	if (req.session.login) {
+		request.post({url: 'http://' + req.session.address + '/api/Encomendas/', form: req.session.login}, function (err, response, body) {
+            if (err) { res.status(400).send() }
+            console.log(JSON.parse(body));
+            console.log(err);
+			var encomendas = JSON.parse(body);
+			var encomendasPendentes = encomendas.filter(function(encomenda){
+				return !(encomenda.Entregue);
+			});
+			res.render('pages/encomendas/index', { encomendas: encomendas, encomendasPendentes: encomendasPendentes });
+		});
+		} else {
         res.redirect('/login');
-    }
+	}
 });
 
 /* GET informacao sobre a encomenda. */
 router.get('/:encomendaID', function (req, res, next) {
     if (req.session.login) {
-        res.render('pages/encomendas/show');
-    } else {
+		request.post({ url: 'http://' + req.session.address + '/api/Encomendas/' + req.params.encomendaID, form: req.session.login }, function (err, response, body) {
+            if (err) { res.status(400).send() }
+            console.log(body);
+            console.log(err);
+			res.render('pages/encomendas/show',{ venda: JSON.parse(body) });
+		});
+		} else {
         res.redirect('/login');
-    }
+	}
 });
 
 module.exports = router;
