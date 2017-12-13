c661c14e8e2a4e828bda1a54f514125beecd3cc3
Bragado <luis_coelho_feita@hotmail.com>
2017-12-13 04:55:11 +0000

    ganhos

--- routes/ganhos.js
+++ routes/ganhos.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+const request = require('request');
 /* GET pagina ganhos. */
 router.get('/', function (req, res, next) {
     if (req.session.login) {
-        res.render('pages/ganhos/index');
+        console.log(req.session.login);
+        request.post({url: 'http://' + req.session.address + '/api/Ganhos/', form: req.session.login}, function (err, response, body) {
+            if (err) { res.status(400).send() }
+            console.log(JSON.parse(body));
+            console.log(err);
+			 
+            res.render('pages/ganhos/index', { ganhos: JSON.parse(body) });
+        });
     } else {
         res.redirect('/login');
     }
 });
+
+
 
 module.exports = router;
